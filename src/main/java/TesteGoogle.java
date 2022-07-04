acacb5c92205728eb99d5c8154bb9fe883ead240
Diego <diegobr@hotmail.com>
2022-07-04 08:06:31 -0300

    Created method teste

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,13 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 	
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 		System.setProperty("webdriver.gecko.firefox", "/home/diego/eclipse-workspace/SeleniumCourse/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
 	}
 
 }
