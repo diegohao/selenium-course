c852d142655cfdcee1f7ad2d38232ff5e6365201
Diego <diegobr@hotmail.com>
2022-07-04 22:38:27 -0300

    Created method to interact with textField and TextArea

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,4 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -9,6 +12,26 @@ public class TesteCampoTreinamento {
 		System.setProperty("webdriver.gecko.firefox", "/home/diego/eclipse-workspace/SeleniumCourse/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.quit();
+	}
+	
+	@Test
+	public void testeTextField() {
+		System.setProperty("webdriver.gecko.firefox", "/home/diego/eclipse-workspace/SeleniumCourse/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
+		Assert.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComTextArea() {
+		System.setProperty("webdriver.gecko.firefox", "/home/diego/eclipse-workspace/SeleniumCourse/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste\nSegunda linha");
+		Assert.assertEquals("Teste\nSegunda linha", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
 		driver.quit();
 	}
 
