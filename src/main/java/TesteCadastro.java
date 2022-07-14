e63f6c2da84db173ee8f8c1c4d4c732dec142b21
Diego <diegobr@hotmail.com>
2022-07-14 17:49:24 -0300

    Created class TesteCadastro

---
+++ src/main/java/TesteCadastro.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TesteCadastro {
+
+	@Test
+	public void deveCadastrarComSucesso() {
+		System.setProperty("webdriver.gecko.firefox", "/home/diego/eclipse-workspace/SeleniumCourse/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Diego");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Henrique");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		new Select(driver.findElement(By.id("elementosForm:escolaridade"))).selectByVisibleText("Superior");
+		new Select(driver.findElement(By.id("elementosForm:esportes"))).selectByVisibleText("Futebol");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		
+		Assert.assertTrue(driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
+		Assert.assertTrue(driver.findElement(By.id("descNome")).getText().endsWith("Diego"));
+		Assert.assertTrue(driver.findElement(By.id("descSobrenome")).getText().endsWith("Henrique"));
+		Assert.assertEquals("Sexo: Masculino", driver.findElement(By.id("descSexo")).getText());
+		Assert.assertEquals("Comida: Pizza", driver.findElement(By.id("descComida")).getText());
+		Assert.assertEquals("Escolaridade: superior", driver.findElement(By.id("descEscolaridade")).getText());
+		Assert.assertEquals("Esportes: Futebol", driver.findElement(By.id("descEsportes")).getText());
+		
+		driver.quit();
+	}
+	
+}
