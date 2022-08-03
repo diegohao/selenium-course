ac43dccc97c87fff5d92d4681635e925d7a87346
Diego <diegobr@hotmail.com>
2022-08-03 00:12:13 -0300

    Test deveInteragiorComFrameEscondido created

--- src/main/java/TesteFramesEJanelas.java
+++ src/main/java/TesteFramesEJanelas.java
@@ -2,9 +2,9 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteFramesEJanelas {
@@ -52,6 +52,17 @@ public class TesteFramesEJanelas {
 		dsl.escrever(By.tagName("textarea"), "Deu certo?");
 		dsl.trocarJanela((String) driver.getWindowHandles().toArray()[0]);
 		dsl.escrever(By.tagName("textarea"), "E agora?");
+	}
+	
+	@Test
+	public void deveInteragiorComFrameEscondido() {
+		WebElement frame = driver.findElement(By.id("frame2"));
+		// Scroll
+		dsl.executarJS("window.scrollBy(0, arguments[0])", frame.getLocation().y);
+		dsl.entrarFrame("frame2");
+		dsl.clicarBotao("frameButton");
+		String msg = dsl.alertaObterTextoEAceita();
+		Assert.assertEquals("Frame OK!", msg);
 	}
 
 }
